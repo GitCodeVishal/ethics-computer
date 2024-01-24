83e779606177d39e2acab37d6760e05fbc0420f0
GitCodeVishal <vkashyap1998@gmail.com>
2024-01-24 16:29:26 +0530

    Styled layout

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import '../styles/Layout.css'; 
+import '../styles/Layout.css';
 import { Outlet } from 'react-router';
 
 const Layout = () => {
@@ -17,6 +17,43 @@ const Layout = () => {
                 </div>
             </header>
             <Outlet />
+            <footer className="footer">
+                <div className="footer-top">
+                    <h2>Your Title</h2>
+                    <div className="social-icons">
+                        <InstagramIcon />
+                        <YouTubeIcon />
+                        <FacebookIcon />
+                    </div>
+                </div>
+
+                <div className="footer-center">
+                    <div className="footer-section">
+                        <h3>Home</h3>
+                        <h3>Book Now</h3>
+                        <h3>Add-ons</h3>
+                        <h3>About Us</h3>
+                    </div>
+                    <div className="footer-section">
+                        <h3>Gallery</h3>
+                        <h3>Refund Policy</h3>
+                        <h3>Privacy Policy</h3>
+                        <h3>Terms and Conditions</h3>
+                    </div>
+                </div>
+
+                <div className="footer-bottom">
+                    <h3>Contact Us:</h3>
+                    <ul>
+                        <li>401, Meta towers, Hitec city
+                            Madhapur, Hyderabad - 500081</li>
+                        <li>+91 9948954545 (WhatsApp only)</li>
+                        <li>admin@bingenbash.com</li>
+                    </ul>
+                </div>
+                <p>2023 © Binge 'n Bash, All Rights Reserved</p>
+            </footer>
+
         </div>
     );
 }
---
+++ src/Components/platinum_hitec_city.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const platinum_hitec_city = () => {
+  return (
+    <div>platinum_hitec_city</div>
+  )
+}
+
+export default platinum_hitec_city
